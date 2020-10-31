ce0f5ccb1cec0592a5184d64d3cc687ec4f26a55
satoshi861 <861oliver@gmail.com>
2020-10-31 21:52:49 +0800

    Initial commit, saving w01~w07 files

---
+++ w07-nav/nav.js
@@ -0,0 +1,34 @@
+function change(index) {
+  //   const p = document.getElementById('show');
+  const p = document.querySelector('#show');
+  console.log('p', p);
+  switch (index) {
+    case 1:
+      p.innerHTML = '<iframe width=100% height=100% src="./w01/index.html" />';
+      break;
+    case 2:
+      p.innerHTML =
+        '<iframe width=100% height=100% src="./w01/pokemon.html" />';
+      break;
+    case 3:
+      p.innerHTML =
+        '<iframe width=100% height=100% src="./w02-imgGallery/imgGallery.html" />';
+      break;
+    case 4:
+      p.innerHTML =
+        '<iframe width=100% height=100% src="./w02-blog/blog.html" />';
+      break;
+    case 5:
+      p.innerHTML =
+        '<iframe width=100% height=100% src="./w03-div/div.html" />';
+      break;
+    case 6:
+      p.innerHTML =
+        '<iframe width=100% height=100% src="./w04-tku60-js/div_tku60.html" />';
+      break;
+    case 7:
+      p.innerHTML =
+        '<iframe width=100% height=100% src="./w07-ms-clone/index.html" />';
+      break;
+  }
+}
